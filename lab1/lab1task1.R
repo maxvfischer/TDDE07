319f97b1a67129c8598de0fb347c4d5e90acf369
Max Fischer <maxfi539@student.liu.se>
2018-03-28 15:11:28 +0200

    Add done task b

--- lab1/lab1task1.R
+++ lab1/lab1task1.R
@@ -40,3 +40,14 @@ plot(theta.n, theta.sd, type='l', xlab='Number of draws', ylab='Standard dev of 
 # b)
 # Use simulation (nDraws = 10000) to compute the posterior probability 
 # Pr(θ < 0.4|y) and compare with the exact value [Hint: pbeta()].
+
+theta.draws <- rbeta(10000, posterior_alpha, posterior_beta) # Draw 10000 numbers
+theta.no_smaller <- ifelse(theta.draws < 0.4, 1, 0) # If draw < 0.4 -> 1, else -> 0
+theta.prob_smaller <- sum(theta.no_smaller)/length(theta.draws) # Calculate probability of drawn number being < 0.4
+print(pbeta(0.4,posterior_alpha, posterior_beta)) # Actual probability of b
+
+# ------------------------------------------------------------------------------------------------- #
+# c) 
+# Compute the posterior distribution of the log-odds φ = log(θ / 1−θ)
+# by simulation (nDraws = 10000). [Hint: hist() and density() might come in handy]
+
